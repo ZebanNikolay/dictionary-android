aed1830ae94f37c20783b1478adda5c0ba0974c0
ZebanNikolay <zebannikolay@gmail.com>
2021-01-10 18:47:41 +0300

    Init project

---
+++ app/src/main/java/com/ncbs/dictionary/presentation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ncbs.dictionary.presentation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ncbs.dictionary.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/ncbs/dictionary/presentation/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.ncbs.dictionary.presentation
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
