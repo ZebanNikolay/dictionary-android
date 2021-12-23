037a2a39915ce6cfa7663d93c57f04f0fe01f8f3
ZebanNikolay <zebannikolay@gmail.com>
2021-12-23 21:09:23 +0200

    Implement appbar

--- app/src/main/java/com/ncbs/dictionary/presentation/MainFragment.kt
+++ app/src/main/java/com/ncbs/dictionary/presentation/MainFragment.kt
@@ -1,12 +1,13 @@
 package com.ncbs.dictionary.presentation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
+import androidx.annotation.MenuRes
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.ncbs.dictionary.R
+import com.ncbs.dictionary.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -16,8 +17,52 @@ class MainFragment : Fragment() {
 
     private val viewModel: MainViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.language_action -> {
+                    showMenu(binding.toolbar.findViewById(R.id.language_action), R.menu.popup_language)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun showMenu(v: View, @MenuRes menuRes: Int) {
+        val popup = PopupMenu(context!!, v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.nv_language_action -> {
+                    true
+                }
+                R.id.ru_language_action -> {
+                    true
+                }
+                R.id.en_language_action -> {
+                    true
+                }
+                else -> false
+            }
+        }
+        popup.show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
