f696eae17b82ecbbda90eadd05bf74a7c60dc52f
ZebanNikolay <zebannikolay@gmail.com>
2022-01-08 17:59:31 +0200

    Remove splash screen

--- app/src/main/java/com/ncbs/dictionary/presentation/MainActivity.kt
+++ app/src/main/java/com/ncbs/dictionary/presentation/MainActivity.kt
@@ -2,14 +2,12 @@ package com.ncbs.dictionary.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ncbs.dictionary.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
